fa4bfb6bbd0bfb373603cd244f816b83441fc1a4
Xinyi Lin <kevinmacbook@Xinyis-MacBook-Pro.local>
2022-10-10 16:32:01 -0400

    Attempt to deploy

--- search/views.py
+++ search/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the home index.")
+    return render(request, 'search/index.html')
