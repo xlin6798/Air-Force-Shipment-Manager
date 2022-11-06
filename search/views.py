b5b1ba5e7007c7b645ca17cb097247f73b8101f3
Xinyi Lin <kevinmacbook@Xinyis-MacBook-Pro.local>
2022-11-06 17:15:06 -0500

    Implement Boostrap 5 and move styling to external css file

--- search/views.py
+++ search/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Product
 
 def index(request):
-    product_list = Product.objects.order_by('-product_un')[:5]
+    product_list = Product.objects.order_by('product_un').all()
     context = {'product_list': product_list}
     return render(request, 'search/index.html', context)
 
