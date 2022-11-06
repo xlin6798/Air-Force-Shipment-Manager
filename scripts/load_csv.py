3aa734630ab31509bbaec3ae010c06c3c76c3bc2
Xinyi Lin <kevinmacbook@Xinyis-MacBook-Pro.local>
2022-11-06 18:39:24 -0500

    Add two new models and import corresponding csv file to each table

--- scripts/load_csv.py
+++ scripts/load_csv.py
@@ -1,7 +1,7 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project_name.settings")
 
-from search.models import Product
+from search.models import Product, SpCode, HazClass
 import csv
 
 with open('scripts/A41.csv') as file:
@@ -21,3 +21,29 @@ with open('scripts/A41.csv') as file:
         product.product_sp=row[5]
         product.product_paragraph=row[6]
         product.save()
+
+with open('scripts/A42.csv') as file:
+    reader = csv.reader(file)
+    next(reader)  # Advance past the header
+
+    SpCode.objects.all().delete()
+
+    for row in reader:
+        print(row)
+        spcode = SpCode()
+        spcode.sp_code=row[0]
+        spcode.sp_desc=row[1]
+        spcode.save()
+
+with open('scripts/HazClass.csv') as file:
+    reader = csv.reader(file)
+    next(reader)  # Advance past the header
+
+    HazClass.objects.all().delete()
+
+    for row in reader:
+        print(row)
+        hazclass = HazClass()
+        hazclass.haz_num =row[0]
+        hazclass.haz_name =row[1]
+        hazclass.save()
--- search/models.py
+++ search/models.py
@@ -1,3 +1,4 @@
+from distutils.spawn import spawn
 from django.db import models
 
 class Product(models.Model):
@@ -8,3 +9,11 @@ class Product(models.Model):
     product_pg = models.CharField(max_length=200)
     product_sp = models.CharField(max_length=200)
     product_paragraph = models.CharField(max_length=200)
+
+class SpCode(models.Model):
+    sp_code = models.CharField(max_length=200)
+    sp_desc = models.CharField(max_length=200)
+
+class HazClass(models.Model):
+    haz_num = models.CharField(max_length=200)
+    haz_name = models.CharField(max_length=200)
