7c98becb59d44cdfe3bd872e6f2595b94e506840
Shuhei Yamaguchi <shuhei@SY-MacbookPro.lan>
2022-11-07 21:34:05 -0500

    simple test case

--- search/tests.py
+++ search/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Product, SpCode, HazClass
 
 # Create your tests here.
+class TestPages(TestCase):
+
+    # Check to see if its lands to correct page
+    def test_NA0331(self):
+        page = self.client.get('/products/NA0331')
+        self.assertEqual(page.status_code, 301)
