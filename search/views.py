628f0ee50f49f6f2a85e3d7227c5c1362eb67b58
Xinyi Lin <kevinmacbook@Xinyis-MacBook-Pro.local>
2022-11-07 21:00:27 -0500

    Update sp code sidebar

--- search/views.py
+++ search/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, SpCode, HazClass
 
 def index(request):
     product_list = Product.objects.order_by('product_un').all()
@@ -8,7 +8,10 @@ def index(request):
 
 def detail(request, un_code):
     product_list = Product.objects.filter(product_un__icontains=un_code)
-    return render(request, 'search/detail.html', {'product_list':product_list})
+    code_list = []
+    for code in product_list[0].product_haz_class.split(','):
+        code_list.append(SpCode.objects.filter(sp_code__icontains=code))
+    return render(request, 'search/detail.html', {'product_list':product_list, 'code_list':code_list})
 
 def shipmentHistory(request):
     return render(request, 'search/shipmentHistory.html')
